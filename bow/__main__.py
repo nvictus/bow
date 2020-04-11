460ac7a07fb879f64ad71f9b4050f76a07392fa4
Nezar Abdennur <nabdennur@gmail.com>
2020-04-11 16:39:21 -0400

    Add parquet loader

--- bow/__main__.py
+++ bow/__main__.py
@@ -1,4 +1,10 @@
-from .bow import IterableParquetFile, format_metadata, format_schema
+import json
+import sys
+import pandas as pd
+from pandas.io.common import get_filepath_or_buffer
+from .bow import (
+    IterableParquetFile, format_metadata, format_schema, to_parquet
+)
 from ._version import __version__
 
 import click
@@ -16,7 +22,7 @@ def cli():
 
 @cli.command()
 @click.argument(
-    "file",
+    "path",
     type=str,
 )
 @click.option(
@@ -24,17 +30,22 @@ def cli():
     help="Print table schema.",
     is_flag=True
 )
-def info(file, schema):
-    f = IterableParquetFile(file)
+def info(path, schema):
+    path, _, _, should_close = get_filepath_or_buffer(path)
+
+    f = IterableParquetFile(path)
     if schema:
         print(format_schema(f.schema))
     else:
         print(format_metadata(f.metadata))
 
+    if should_close:
+        path.close()
+
 
 @cli.command()
 @click.argument(
-    "file",
+    "path",
     type=str,
 )
 @click.option(
@@ -53,12 +64,86 @@ def info(file, schema):
     is_flag=True,
     help="Include index in output."
 )
-def cat(file, sep, header, index):
-    f = IterableParquetFile(file)
+def cat(path, sep, header, index):
+    path, _, _, should_close = get_filepath_or_buffer(path)
+
+    f = IterableParquetFile(path)
     n = 0
     for chunk in f:
         print(chunk.to_csv(sep=sep, index=index, header=header))
         n += len(chunk)
+    
+    if should_close:
+        path.close()
 
+
+@cli.command()
+@click.argument(
+    "path",
+    type=str
+)
+@click.argument(
+    "outpath",
+    type=str
+)
+@click.option(
+    "-ic", "--input-chunksize",
+    type=int,
+    default=4096,
+    help="Input buffer size in bytes"
+)
+@click.option(
+    "-oc", "--output-chunksize",
+    type=int,
+    default=1000000,
+    help="Output row group size (# of rows)"
+)
+@click.option(
+    "-s", "--sep",
+    type=str,
+    default="\t",
+    help="Output field separator [Default: tab]. "
+)
+@click.option(
+    "-H", "--header",
+    is_flag=True,
+    help="Input file contains a header."
+)
+@click.option(
+    "-i", "--index",
+    is_flag=True,
+    help="Set one of the input columns to be the index."
+)
+@click.option(
+    "-k", "--schema",
+    type=str,
+    help="Provide data types for columns as a JSON string."
+)
+def load(path, outpath, sep, header, index, schema, input_chunksize, output_chunksize):
+    if path == '-':
+        path = sys.stdin
+    
+    path, _, _, _ = get_filepath_or_buffer(path)
+
+    if schema is not None:
+        schema = json.loads(schema)
+
+    if header:
+        header = 'infer'
+    else:
+        header = None
+    
+    it = pd.read_csv(
+        path, 
+        sep=sep, 
+        header=header, 
+        dtype=schema, 
+        iterator=True, 
+        chunksize=input_chunksize
+    )
+
+    to_parquet(
+        it, outpath, row_group_size=output_chunksize, compression='snappy'
+    )
 
 cli()
--- bow/bow.py
+++ bow/bow.py
@@ -2,6 +2,7 @@ import json
 import sys
 
 import pandas as pd
+import pyarrow as pa
 import pyarrow.parquet as pq
 
 pd.set_option("display.max_columns", None)
@@ -26,9 +27,92 @@ class IterableParquetFile(pq.ParquetFile):
         return rg.to_pandas()
 
 
+def read_parquet(filepath, columns=None, iterator=False, **kwargs):
+   """
+   Load DataFrames from Parquet files, optionally in pieces.
+
+   Parameters
+   ----------
+   filepath : str, pathlib.Path, pyarrow.NativeFile, or file-like object
+       Readable source. For passing bytes or buffer-like file containing a
+       Parquet file, use pyarorw.BufferReader
+   columns: list
+       If not None, only these columns will be read from the row groups. A
+       column name may be a prefix of a nested field, e.g. 'a' will select
+       'a.b', 'a.c', and 'a.d.e'
+   iterator : boolean, default False
+       Return an iterator object that yields row group DataFrames and
+       provides the ParquetFile interface.
+   use_threads : boolean, default True
+       Perform multi-threaded column reads
+   memory_map : boolean, default True
+       If the source is a file path, use a memory map to read file, which can
+       improve performance in some environments
+
+   Returns
+   -------
+   DataFrame or ParquetFileIterator
+
+   """
+   use_threads = kwargs.pop('use_threads', True)
+
+   if not iterator:
+       return pd.read_parquet(filepath, columns=columns,
+                              use_threads=use_threads, **kwargs)
+   else:
+       return IterableParquetFile(filepath, **kwargs)
+
+
+def to_parquet(pieces, outpath, row_group_size=None, compression='snappy',
+              use_dictionary=True, version=None, **kwargs):
+   """
+   Save an iterable of dataframe chunks to a single Apache Parquet file. For
+   more info about Parquet, see https://arrow.apache.org/docs/python/parquet.html.
+
+   Parameters
+   ----------
+   pieces : DataFrame or iterable of DataFrame
+       Chunks to write
+   outpath : str
+       Path to output file
+   row_group_size : int
+       Number of rows per row group
+   compression : {'snappy', 'gzip', 'brotli', 'none'}, optional
+       Compression algorithm. Can be set on a per-column basis with a
+       dictionary of column names to compression lib.
+   use_dictionary : bool, optional
+       Use dictionary encoding. Can be set on a per-column basis with a list
+       of column names.
+
+   See also
+   --------
+   pyarrow.parquet.write_table
+   pyarrow.parquet.ParquetFile
+   fastparquet
+
+   """
+   if isinstance(pieces, pd.DataFrame):
+       pieces = (pieces,)
+
+   try:
+       for i, piece in enumerate(pieces):
+           table = pa.Table.from_pandas(piece, preserve_index=False)
+           if i == 0:
+               writer = pa.parquet.ParquetWriter(
+                   outpath,
+                   table.schema,
+                   compression=compression,
+                   use_dictionary=use_dictionary,
+                   version=version,
+                   **kwargs)
+           writer.write_table(table, row_group_size=row_group_size)
+   finally:
+       writer.close()
+
+
 def format_schema(schema):
-    return f.schema.to_arrow_schema()
+    return schema.to_arrow_schema()
 
 
 def format_metadata(metadata):
-    return f.metadata
+    return metadata
